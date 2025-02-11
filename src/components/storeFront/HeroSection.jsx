d1c564d91275511c4dd942d6c97dbac784db0db5
spandan <spandan7b@gmail.com>
2025-02-11 22:40:28 +0530

    hero section start

---
+++ src/components/storeFront/HeroSection.jsx
@@ -0,0 +1,16 @@
+function HeroSection() {
+  return (
+    <section className="hero-section">
+      <h2>Summer styles are finally here</h2>
+      <div>
+        This year, our new summer collection will be your haven from the world's
+        harsh elements.
+      </div>
+      <div>
+        <button>Shop Now</button>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/storeFront/StoreFront.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "./HeroSection";
+
+function StoreFront() {
+  return (
+    <div className="store-front">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default StoreFront;
